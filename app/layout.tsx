5c6cb3e037e781aaabf7504f9a92650918a056ce
Trishane <trishaneww@gmail.com>
2024-11-20 21:59:47 -0500

    created initial folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Brawler } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import { cn } from "@/lib/utils";
+
+const inter = Brawler({ weight: ["400", "700"], subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Figment Studios",
+  description:
+    "Grow your business with a custom website that sets you apart in today's digital landscape. A professionally designed site tailored to your brand not only enhances your online presence but also drives traffic, engages customers, and boosts conversions.",
+  icons: {
+    icon: "/assets/icons/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        className={cn(
+          "min-h-screen antialiased",
+          inter.className
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="light">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+
+
+const Home = () => {
+  return (
+    <div className="flex flex-col bg-slate-50 scroll-smooth">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
